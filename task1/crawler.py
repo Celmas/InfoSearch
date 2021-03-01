7f21d5d866f2215972c324f2c84ab3bff10f56b2
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 17:16:44 +0300

    write files to directory, removed useless comments

--- task1/crawler.py
+++ task1/crawler.py
@@ -1,5 +1,5 @@
-# getting the data
 import requests
+import os
 from lxml import etree
 from io import StringIO
 
@@ -9,16 +9,16 @@ def parse(url):
     return resp.text
 
 
-# get html from site and write to local file
 html = parse('https://www.notebookcheck-ru.com/Novosti.news.0.html?&items_per_page=100')
 htmlparser = etree.HTMLParser()
 tree = etree.parse(StringIO(html), htmlparser)
 links = tree.xpath('//a[@class="introa_large introa_news"]/@href')
 
-index = open("index.txt", "w+", encoding='utf-8')
+os.makedirs("data", exist_ok=True)
+index = open("data/index.txt", "w+", encoding='utf-8')
 for i, link in enumerate(links):
     html_source = parse(link)
-    with open(str.format("{}.html".format(i)), 'w', encoding='utf-8') as f:
+    with open("data/" + str.format("{}.html".format(i)), 'w', encoding='utf-8') as f:
         f.write(html_source)
     index.write("{} - {}\n".format(i, link))
 
